d1f12368090c4d55cc1053c6fbbe49e44c6b41e6
Andrew Hill <anjo.hill@gmail.com>
2017-04-30 19:15:33 -0600

    Working through Unity Tutorials

---
+++ Assets/scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log ("Trigger");
+		GameObject.Find ("LevelManager").GetComponent<LevelManager> ().LoadLevel ("Win");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log ("Collision");
+	}
+}
---
+++ Assets/scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	private float mousePosInBlocks;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetMousePosInBlocks (Input.mousePosition.x / Screen.width * 16 - 8);
+		Debug.Log (mousePosInBlocks);
+
+		Vector3 paddlePos = new Vector3(mousePosInBlocks, this.transform.position.y, this.transform.position.z);
+
+		paddlePos.x = Mathf.Clamp (paddlePos.x, -7.5f, 7.5f);
+		this.transform.position = paddlePos;
+		
+	}
+
+	void SetMousePosInBlocks(float pos) {
+		mousePosInBlocks = pos;
+	}
+}
